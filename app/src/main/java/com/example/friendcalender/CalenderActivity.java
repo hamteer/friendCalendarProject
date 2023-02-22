0ac2e7996f639efa4226055967868a8634d3a289
KerimAgalarov <102965396+KerimAgalarov@users.noreply.github.com>
2023-02-22 18:50:39 +0100

    Add settings-icon to ActionBar

--- app/src/main/java/com/example/friendcalender/CalenderActivity.java
+++ app/src/main/java/com/example/friendcalender/CalenderActivity.java
@@ -1,21 +1,29 @@
 package com.example.friendcalender;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class CalenderActivity extends AppCompatActivity {
 
-    private ImageView viewSettings;
+    private ImageView settings_action_bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calender);
-        viewSettings= findViewById(R.id.viewSettings);
-        viewSettings.setOnClickListener(view -> {
-            startActivity(new Intent(CalenderActivity.this, SettingsActivity.class));
-        });
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            actionBar.setCustomView(R.layout.action_bar);
+            settings_action_bar = actionBar.getCustomView().findViewById(R.id.settings_action_bar);
+
+            settings_action_bar.setOnClickListener(view -> {
+                startActivity(new Intent(CalenderActivity.this, SettingsActivity.class));
+            });
+        }
     }
 }
