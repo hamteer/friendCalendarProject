5b0aae43c0fa22c0e0ce3e2225d03d700f4d2a8a
Daniel <dak2791@thi.de>
2023-03-18 14:14:10 +0100

    added filter to SingleDayActivity.java so only Events of selectedDay get displayed

--- app/src/main/java/com/frcal/friendcalender/Activities/SingleDayActivity.java
+++ app/src/main/java/com/frcal/friendcalender/Activities/SingleDayActivity.java
@@ -2,8 +2,6 @@ package com.frcal.friendcalender.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,13 +12,20 @@ import com.frcal.friendcalender.Activities.RecyclerView.DateListRecyclerAdapter;
 import com.frcal.friendcalender.DataAccess.EventManager;
 import com.frcal.friendcalender.DatabaseEntities.CalenderEvent;
 import com.frcal.friendcalender.R;
+import com.google.api.client.util.DateTime;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
+
+import org.threeten.bp.LocalDate;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class SingleDayActivity extends AppCompatActivity implements DateListRecyclerAdapter.DateListAdapterListener, EventManager.EventManagerListener {
     TextView headline;
     RecyclerView eventList;
     EventManager eventManager;
     DateListRecyclerAdapter dateListAdapter;
+    CalendarDay selectedDate;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,7 +46,7 @@ public class SingleDayActivity extends AppCompatActivity implements DateListRecy
 
         // set headline to display selected day:
         Bundle extras = getIntent().getExtras();
-        CalendarDay selectedDate = (CalendarDay) extras.get("SELECTED_DATE");
+        selectedDate = (CalendarDay) extras.get("SELECTED_DATE");
         String selectedDateString = selectedDate.getDay() + "." + selectedDate.getMonth() + "." + selectedDate.getYear();
         headline.setText("Termine am " + selectedDateString);
 
@@ -54,7 +59,20 @@ public class SingleDayActivity extends AppCompatActivity implements DateListRecy
     // TODO: filter events to show only events of selected day
     @Override
     public void onEventListUpdated() {
-        dateListAdapter.setEvents(eventManager.getEvents());
+
+        String day = (selectedDate.getDay()<10)? "0" + selectedDate.getDay() : String.valueOf(selectedDate.getDay());
+        String month = (selectedDate.getMonth()<10)? "0" + selectedDate.getMonth() : String.valueOf(selectedDate.getMonth());
+
+        DateTime date = new DateTime(selectedDate.getYear() + "-" + month + "-" + day);
+        // check if selectedDay equals startTime-Day => add event to list which gets displayed
+        ArrayList<CalenderEvent> eventsOfSelectedDay = new ArrayList<>();
+        for (CalenderEvent event: eventManager.getEvents()){
+            if(event.startTime.toString().substring(0,10).equals(date.toString().substring(0,10))){
+                eventsOfSelectedDay.add(event);
+            }
+        }
+        dateListAdapter.setEvents(eventsOfSelectedDay);
+        //dateListAdapter.setEvents(eventManager.getEvents());
     }
 
     @Override
