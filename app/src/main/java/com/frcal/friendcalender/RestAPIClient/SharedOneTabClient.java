127d94e3faa40ee525e9dc2e99cfbfd791f88dc8
Kamker <102965396+AKamKer@users.noreply.github.com>
2023-03-26 17:34:01 +0200

    Add comment

--- app/src/main/java/com/frcal/friendcalender/RestAPIClient/SharedOneTabClient.java
+++ app/src/main/java/com/frcal/friendcalender/RestAPIClient/SharedOneTabClient.java
@@ -4,11 +4,16 @@ package com.frcal.friendcalender.RestAPIClient;
 import com.google.android.gms.auth.api.identity.SignInClient;
 
 
+/* You can only Sign-out if you have the same SignInClient-Object. Therfore we need the Singleton "SharedOneTabClient".
+   So SharedOneTabClient.getInstance() creates this object only one time. You can access this object and its instance variable oneTabClient
+   from multiple Activitys and do a sign in or out. 
+*/
 
 public class SharedOneTabClient {
     private static SharedOneTabClient instance = null;
     private SignInClient oneTapClient = null;
-
+    
+    //Create instatnce only one time
     public static SharedOneTabClient getInstance() {
         if (instance == null) {
             instance = new SharedOneTabClient();
